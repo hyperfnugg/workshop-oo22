47be39567679cb100e69f061f9279117079dda28
Erlend Viddal <esv@scienta.no>
2022-10-12 17:06:23 +0200

    Chance with equals and hashCode

--- src/main/kotlin/no/scienta/workshop/oooct22/probability/Chance.kt
+++ src/main/kotlin/no/scienta/workshop/oooct22/probability/Chance.kt
@@ -1,4 +1,30 @@
-package no.scienta.workshop.oooct22.probability
+import java.math.BigDecimal
+import java.math.RoundingMode
 
-class Chance {
+//Understands the likelihood of something occuring
+class Chance private constructor(private val fraction: BigDecimal) {
+    init {
+        require(fraction.scale() == SCALE)
+        require(fraction >= BigDecimal.ZERO)
+        require(fraction <= BigDecimal.ONE)
+    }
+
+    override fun equals(other: Any?) =
+        this === other || other is Chance && this.equals(other)
+
+    private fun equals(other: Chance) =
+        this.fraction == other.fraction
+
+    override fun hashCode() =
+        fraction.hashCode()
+
+
+    companion object {
+        private const val SCALE = 5
+        infix fun Number.outOf(total: Number) =
+            (this.toDouble() / total.toDouble())
+                .let(Double::toBigDecimal)
+                .setScale(SCALE, RoundingMode.HALF_UP)
+                .let(::Chance)
+    }
 }
--- src/test/kotlin/no/scienta/workshop/oooct22/probability/ChanceTest.kt
+++ src/test/kotlin/no/scienta/workshop/oooct22/probability/ChanceTest.kt
@@ -1,12 +1,40 @@
 package no.scienta.workshop.oooct22.probability
 
+import Chance.Companion.outOf
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class ChanceTest {
+
+    companion object {
+        private val CERTAIN = 1 outOf 1
+        private val IMPOSSIBLE = 0 outOf 1
+        private val LIKELY = 3 outOf 4
+        private val UNLIKELY = 1 outOf 4
+        private val EQUALLY_LIKELY = 1 outOf 2
+    }
+
+
     @Test
-    fun testKotlin(){
-        val c = Chance()
-        assertEquals(c,c)
+    fun `invalid fractions`() {
+        assertThrows<IllegalArgumentException> { 12 outOf 10 }
+        assertThrows<IllegalArgumentException> { 12 outOf 0 }
+        assertThrows<IllegalArgumentException> { -2 outOf 10 }
+    }
+
+    @Test
+    fun equality() {
+        assertEquals(CERTAIN, CERTAIN)
+        assertEquals(EQUALLY_LIKELY, 1 outOf 2)
+        assertNotEquals(LIKELY, UNLIKELY)
+        assertNotEquals(IMPOSSIBLE, null)
+    }
+
+    @Test
+    fun hash() {
+        assertEquals(CERTAIN.hashCode(), CERTAIN.hashCode())
+        assertEquals(EQUALLY_LIKELY.hashCode(), (1 outOf 2).hashCode())
     }
 }
