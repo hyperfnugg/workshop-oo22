e4e71cbe1cbe432c6fa2e999343968a18754c9b1
Erlend Viddal <esv@scienta.no>
2022-10-12 17:01:25 +0200

    Choose JUnit5 for now

--- src/test/kotlin/no/scienta/workshop/oooct22/MainTest.kt
+++ src/test/kotlin/no/scienta/workshop/oooct22/MainTest.kt
@@ -1,22 +1,15 @@
 package no.scienta.workshop.oooct22
 
-import org.assertj.core.api.Assertions.assertThat
-import kotlin.test.Test
-import kotlin.test.assertTrue
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
 
 class MainTest {
 
     @Test
     fun testKotlin(){
-        assertTrue ("The developer has fixed the example test") {
-            false
+        assertTrue {
+            true
         }
-    }
-
-    @Test
-    fun testAssertJ() {
-        assertThat(false)
-            .describedAs("The developer has fixed the example test")
-            .isTrue
     }
 }
