0a81551cfa4c61acc9fb5e0e16aae2f4073f67fd
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2022-10-13 08:47:30 +0200

    Split us up

--- src/main/kotlin/no/scienta/workshop/oooct22/quantity/Unit.kt
+++ src/main/kotlin/no/scienta/workshop/oooct22/quantity/Unit.kt
@@ -2,15 +2,21 @@
 
 package no.scienta.workshop.oooct22.quantity
 
-sealed class Unit<D : Dimension>(private val ratio: Int, private val unit: Unit<D>? = null, val dimension: D = unit?.dimension!!) {
+import java.lang.IllegalStateException
 
-    override fun toString(): String = "${javaClass.simpleName}/${dimension}"
+sealed class Unit<D : Dimension>(
+    private val ratio: Int,
+    private val unit: Unit<D>? = null,
+    val dimension: D = unit?.dimension ?: throw IllegalStateException("The base unit needs an explicit dimension"),
+) {
 
     internal fun comparesWith(unit: Unit<*>): Boolean = dimension === unit.dimension
 
     internal fun baseAmount(amount: Int) = amount * chainedRatio
 
     private val chainedRatio: Int get() = this.ratio * (unit?.chainedRatio ?: 1)
+
+    override fun toString(): String = "${javaClass.simpleName}/${dimension}"
 }
 
 
